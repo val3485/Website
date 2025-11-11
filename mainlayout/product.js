5fbf785e4e9303152da323b3318ba3359ae64db7
Shantee Naje <shanteenaje07@gmail.com>
2025-11-11 18:20:16 +0800

    wait

--- mainlayout/product.js
+++
@@ -1,6 +0,0 @@
-// You can add interactive behavior later
-// For example, simulate an "Add to Cart" click
-
-document.querySelector('.cart').addEventListener('click', () => {
-  alert('Cold Brew with Milk added to your cart!');
-});
